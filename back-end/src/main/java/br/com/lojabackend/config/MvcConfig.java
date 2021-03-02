00a65e9c78967e7b5feb0b00464d0d14a27f43b4
Tales Victor Gonçalves de Santana <tales.santana@aluno.unievangelica.edu.br>
2021-03-02 16:12:47 -0300

    Adicionando configurações de cors

--- back-end/src/main/java/br/com/lojabackend/config/MvcConfig.java
+++ back-end/src/main/java/br/com/lojabackend/config/MvcConfig.java
@@ -1,4 +1,17 @@
 package br.com.lojabackend.config;
 
-public class MvcConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
 }
